b696252db51eaa71dbcb0a05aaea15338a9c63b6
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2021-09-11 18:33:18 +0500

    Add project files.

---
+++ PDFGenerator/Controllers/PDFController.cs
@@ -0,0 +1,96 @@
+﻿using PDFGenerator.Models;
+using Rotativa.MVC;
+using System;
+using System.Web.Mvc;
+
+namespace PDFGenerator.Controllers
+{
+    public class PDFController : Controller
+    {
+        // GET: PDF
+        //[Route("/GetPDF")]
+        public ActionResult GetInvoice()
+        {
+            InvoiceModel model = new InvoiceModel
+            {
+                Address = "JEEDAH",
+                AmountInWords = "Twanty Five Saudi Riyal Only",
+                Balance = 0,
+                CashRecevied = 0,
+                ContactNumber = "12345",
+                CustomerNumber = "DANA SUPER MARKET NUZAL",
+                Discount = 0,
+                InvoiceTime = DateTime.Now,
+                NetTotal = 25,
+                OldBalance = 0,
+                SalesMan = "Ali",
+                TotalBeforeTax = 25,
+                VAT = "",
+                VATAmount = 0,
+                VoucherNumber = "302"
+            };
+            InvoiceDetailModel detail1 = new InvoiceDetailModel
+            {
+                ItemName = "Best Royal Date",
+                NetTotal = 25,
+                VATAmount = 0,
+                Quantity = 5,
+                Rate = 5,
+                Total = 25
+            };
+            InvoiceDetailModel detail2 = new InvoiceDetailModel
+            {
+                ItemName = "Best Royal Date",
+                NetTotal = 25,
+                VATAmount = 0,
+                Quantity = 5,
+                Rate = 5,
+                Total = 25
+            };
+            InvoiceDetailModel detail3 = new InvoiceDetailModel
+            {
+                ItemName = "Best Royal Date",
+                NetTotal = 25,
+                VATAmount = 0,
+                Quantity = 5,
+                Rate = 5,
+                Total = 25
+            };
+            InvoiceDetailModel detail4 = new InvoiceDetailModel
+            {
+                ItemName = "Best Royal Date",
+                NetTotal = 25,
+                VATAmount = 0,
+                Quantity = 5,
+                Rate = 5,
+                Total = 25
+            };
+            InvoiceDetailModel detail5 = new InvoiceDetailModel
+            {
+                ItemName = "Best Royal Date",
+                NetTotal = 25,
+                VATAmount = 0,
+                Quantity = 5,
+                Rate = 5,
+                Total = 25
+            };
+            model.Details.Add(detail1);
+            model.Details.Add(detail2);
+            model.Details.Add(detail3);
+            model.Details.Add(detail4);
+            model.Details.Add(detail5);
+            return View(model);
+        }
+        public ActionResult GetInvoicePrint()
+        {
+            Rotativa.Core.DriverOptions options = new Rotativa.Core.DriverOptions
+            {
+                PageMargins = new Rotativa.Core.Options.Margins(0, 0, 0, 0),
+                PageSize = Rotativa.Core.Options.Size.A4,
+                PageOrientation = Rotativa.Core.Options.Orientation.Portrait
+            };
+            return new ActionAsPdf("GetInvoice")
+            { FileName = $"Invoice_{DateTime.Now:ddMMyyyyHmmss}.pdf", RotativaOptions = options };
+        }
+    }
+}
---
+++ PDFGenerator/Models/InvoiceModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PDFGenerator.Models
+{
+    public class InvoiceModel
+    {
+        public InvoiceModel()
+        {
+            Details = new List<InvoiceDetailModel>();
+        }
+        public string VoucherNumber { get; set; }
+        public DateTime InvoiceTime { get; set; }
+        public string SalesMan { get; set; }
+        public string CustomerNumber { get; set; }
+        public string ContactNumber { get; set; }
+        public string VAT { get; set; }
+        public string Address { get; set; }
+        public decimal TotalBeforeTax { get; set; }
+        public decimal VATAmount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal NetTotal { get; set; }
+        public decimal OldBalance { get; set; }
+        public decimal CashRecevied { get; set; }
+        public decimal Balance { get; set; }
+        public string AmountInWords { get; set; }
+        public List<InvoiceDetailModel> Details { get; set; }
+    }
+
+    public class InvoiceDetailModel
+    {
+        public string ItemName { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Total { get; set; }
+        public decimal VATAmount { get; set; }
+        public decimal NetTotal { get; set; }
+    }
+}
